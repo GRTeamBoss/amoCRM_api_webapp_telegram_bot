2dc0e8b8c49c172c29698fbe3430be298ad4edc6
GRTeamBoss <rabbitwow97@gmail.com>
2025-08-23 21:46:20 +0500

    delete files for python project

--- .netlify/functions/config/config.js
+++
@@ -1,9 +0,0 @@
-import { configDotenv } from "dotenv";
-
-export class Config {
-  constructor() {
-    configDotenv();
-    this.TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
-    this.ADMINS = process.env.ADMINS.split(",").map(Number);
-  }
-}
